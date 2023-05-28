ff008e5f79e1a329166cb1590ab89e88767df8cc
johnnyvish <johnny.vishnevskiy@gmail.com>
2023-05-28 00:00:39 -0400

    Initial commit

---
+++ main.js
@@ -0,0 +1,278 @@
+import * as THREE from 'three';
+import { RenderPass } from 'three/examples/jsm/postprocessing/RenderPass';
+import {AfterimagePass} from 'three/examples/jsm/postprocessing/AfterimagePass';
+import { EffectComposer } from 'three/examples/jsm/postprocessing/EffectComposer';
+import { FontLoader } from 'three/examples/jsm/loaders/FontLoader';
+import { TextGeometry } from 'three/examples/jsm/geometries/TextGeometry';
+import './style.css';
+
+const STAR_SPREAD = 1000;
+const STAR_COUNT = 30000;
+const ASTEROID_START_POSITION = -50;
+const EARTH_POSITION = 400;
+const SUN_POSITION = 1000;
+const STAR_CENTER = 200;
+const TEXT_VISIBILITY_THRESHOLD = 30;
+
+let scrollY = 0;
+let maxSpeed = 10;
+let dampingFactor = 0.85;
+let yearsGraphic = [];
+
+function createYears(scene) {
+  let loader = new FontLoader();
+
+  loader.load('Bebas.json', function (font) {
+    const yearCount = (2050 - 1940) / 10 + 1;
+
+    for (let i = 0; i < yearCount; i++) {
+      let year = 1940 + i * 10;
+      if (year == 2050) year = "END";
+
+      let textGeo = new TextGeometry(String(year), {
+        font: font,
+        size: 2,
+        height: 0.1,
+        curveSegments: 10,
+        bevelEnabled: true,
+        bevelThickness: 0.3,
+        bevelSize: 0.01,
+        bevelOffset: 0,
+        bevelSegments: 5
+      });
+
+      let textMaterial = new THREE.MeshPhongMaterial({
+        color: 'orange',
+        specular: 0xffffff,
+        shininess: 200,
+        transparent: true,
+        opacity: 0
+      });
+
+      let text = new THREE.Mesh(textGeo, textMaterial);
+      text.position.y = -1;
+      text.position.x = 1.9;
+      text.position.z = (ASTEROID_START_POSITION + i / (yearCount - 1) * (EARTH_POSITION - ASTEROID_START_POSITION) - 30);
+      text.rotation.y = Math.PI;
+      text.offset = 2;
+      scene.add(text);
+
+      yearsGraphic.push(text);
+    }
+  });
+}
+
+function updateYears(asteroid) {
+  yearsGraphic.forEach((element) => {
+    const distance = asteroid.position.distanceTo(element.position);
+
+    if (distance < TEXT_VISIBILITY_THRESHOLD) {
+      element.material.opacity = 1 - (distance / TEXT_VISIBILITY_THRESHOLD);
+    } else {
+      element.material.opacity = 0;
+    }
+    element.material.needsUpdate = true;
+  });
+}
+
+function createStars(scene) {
+  const starsGeometry = new THREE.BufferGeometry();
+  const starsMaterial = new THREE.PointsMaterial({ color: 0xFFFFFF, size: 0.5 });
+
+  const starPoints = [];
+
+  for (let i = 0; i < STAR_COUNT; i++) {
+      const x = THREE.MathUtils.randFloatSpread(STAR_SPREAD);
+      const y = THREE.MathUtils.randFloatSpread(STAR_SPREAD);
+      const z = THREE.MathUtils.randFloatSpread(STAR_SPREAD) + STAR_CENTER;
+
+      starPoints.push(x, y, z);
+  }
+
+  starsGeometry.setAttribute('position', new THREE.Float32BufferAttribute(starPoints, 3));
+
+  const stars = new THREE.Points(starsGeometry, starsMaterial); 
+
+  scene.add(stars);
+}
+
+function createSun(scene, loader) {
+  const sunGeometry = new THREE.SphereGeometry(10, 64, 64);
+  const sunTexture = loader.load('/sun.jpeg');
+  const sunMaterial = new THREE.MeshBasicMaterial({ map: sunTexture });
+
+  const sun = new THREE.Mesh(sunGeometry, sunMaterial);
+  sun.position.set(0,0, SUN_POSITION);
+  scene.add(sun);
+
+  const sunLight = new THREE.DirectionalLight('white', 1);
+  sunLight.position.set(0,0,SUN_POSITION);
+  scene.add(sunLight);
+}
+
+function createEarth(scene, loader) {
+  const earthGeometry = new THREE.SphereGeometry(1, 64, 64);
+  const earthTexture = loader.load('/earth.jpeg');
+  const earthMaterial = new THREE.MeshPhongMaterial({ map: earthTexture });
+
+  const earth = new THREE.Mesh(earthGeometry, earthMaterial);
+  earth.position.set(0, 0, EARTH_POSITION);
+  scene.add(earth);
+
+  return earth;
+}
+
+function createAsteroid(scene, loader) {
+  const asteroidGeometry = new THREE.SphereGeometry(0.2,5,8);
+  const asteroidTexture = loader.load('/asteroid.jpeg');
+  const asteroidMaterial = new THREE.MeshPhongMaterial({ map: asteroidTexture });
+
+  const asteroid = new THREE.Mesh(asteroidGeometry, asteroidMaterial);
+  asteroid.position.set(0,0,ASTEROID_START_POSITION);
+  scene.add(asteroid);
+
+  return asteroid;
+}
+
+function calculateMoveAmount() {
+  scrollY *= dampingFactor;
+  let moveAmount = scrollY / 1000;
+
+  if (Math.abs(moveAmount) > maxSpeed) {
+    moveAmount = Math.sign(moveAmount) * maxSpeed;
+  }
+  
+  return moveAmount;
+}
+
+function updatePosition(asteroid) {
+  const moveAmount = calculateMoveAmount();
+  updateAsteroidPosition(asteroid, moveAmount);
+  updateTimelinePosition(asteroid);
+}
+
+function updateAsteroidPosition(asteroid, moveAmount) {
+  asteroid.position.z += moveAmount;
+
+  if (asteroid.position.z < ASTEROID_START_POSITION){
+    asteroid.position.z = ASTEROID_START_POSITION;
+  }
+
+  if (asteroid.position.z > EARTH_POSITION){
+    asteroid.position.z = EARTH_POSITION;
+  }
+}
+
+function updateTimelinePosition(asteroid) {
+  const timelineMargin = 3;
+  const timelineWidth = 100 - 2 * timelineMargin;
+
+  const normalizedZ = timelineMargin + (timelineWidth * ((asteroid.position.z - ASTEROID_START_POSITION) / (EARTH_POSITION - ASTEROID_START_POSITION)));
+
+  document.getElementById("asteroid").style.left = `${normalizedZ}%`;
+  document.getElementById("asteroid").style.transform = `rotate(${normalizedZ*5}deg)`;
+}
+
+function updateCameraPosition(camera, asteroid, mouse) {
+  camera.position.x = asteroid.position.x - 2 + mouse.x * 0.1;
+  camera.position.y = asteroid.position.y + mouse.y * 0.1;
+  camera.position.z = asteroid.position.z - 4;
+}
+
+function updateAsteroidRotation(asteroid) {
+  asteroid.rotation.x += 0.05;
+}
+
+function updateEarthRotation(earth) {
+  earth.rotation.y += 0.01;
+}
+
+function lookAtEarth(camera, earth) {
+  camera.lookAt(earth.position.x-1, earth.position.y, earth.position.z);
+}
+
+function onWindowResize(camera, renderer) {
+  camera.aspect = window.innerWidth / window.innerHeight;
+  camera.updateProjectionMatrix();
+  renderer.setSize(window.innerWidth, window.innerHeight);
+}
+
+function generateLoremIpsum() {
+  const words = ['lorem', 'ipsum', 'dolor', 'sit', 'amet', 'consectetur', 'adipiscing', 'elit'];
+  let result = '';
+
+  for (let i = 0; i < 100; i++) {
+      result += words[Math.floor(Math.random() * words.length)] + ' ';
+  }
+
+  return result.trim() + '.';
+}
+
+function animate(composer, scene, camera, asteroid, earth, mouse) {
+  requestAnimationFrame(() => animate(composer, scene, camera, asteroid, earth, mouse));
+
+  updatePosition(asteroid);
+  updateYears(asteroid);
+  updateEarthRotation(earth);
+  updateAsteroidRotation(asteroid);
+  updateCameraPosition(camera, asteroid, mouse);
+  lookAtEarth(camera, earth);
+
+  composer.render(scene, camera);
+}
+
+function main() {
+  const scene = new THREE.Scene();
+  const loader = new THREE.TextureLoader();
+  createStars(scene);
+  createSun(scene, loader);
+  createYears(scene);
+  
+  const asteroid = createAsteroid(scene, loader);
+  const earth = createEarth(scene, loader);
+
+  const ambientLight = new THREE.AmbientLight(0x404040, 10);
+  scene.add(ambientLight);
+
+  const camera = new THREE.PerspectiveCamera(45, window.innerWidth / window.innerHeight, 0.1, 1000);
+  camera.position.set(0, 0, 5);
+
+  const canvas = document.querySelector(".webgl");
+  const renderer = new THREE.WebGLRenderer({ canvas, antialias: true });
+  renderer.setSize(window.innerWidth, window.innerHeight);
+  renderer.setPixelRatio(window.devicePixelRatio);
+
+  const renderScene = new RenderPass(scene, camera);
+  const composer = new EffectComposer(renderer);
+
+  const afterimagePass = new AfterimagePass();
+  afterimagePass.uniforms["damp"].value = 0.5;
+
+  composer.addPass(renderScene);
+  composer.addPass(afterimagePass);
+
+  const mouse = { x: 0, y: 0 };
+  const infoContainer = document.querySelector('.info-text-container');
+
+  let loremIpsumText = '';
+  for (let i = 0; i < 50; i++) {
+      loremIpsumText += '<p>' + generateLoremIpsum() + '</p>';
+  }
+
+  document.getElementById('infoText').innerHTML = loremIpsumText;
+
+  window.addEventListener('mousemove', function(event) {
+    mouse.x = (event.clientX / window.innerWidth) * 2 - 1;
+    mouse.y = -(event.clientY / window.innerHeight) * 2 + 1;
+  });
+  window.addEventListener('resize', () => onWindowResize(camera, renderer), false);
+
+  window.addEventListener('wheel', function(e) {
+    scrollY += e.deltaY;
+   
+  });
+
+  animate(composer, scene, camera, asteroid, earth, mouse);
+}
+
+main();
