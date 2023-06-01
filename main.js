0374329bc3194ab1a7f354b85d43e0acd10cb3a0
johnnyvish <johnny.vishnevskiy@gmail.com>
2023-06-01 19:21:50 -0400

    update

--- main.js
+++ main.js
@@ -7,49 +7,54 @@ import { FontLoader } from 'three/examples/jsm/loaders/FontLoader';
 import { TextGeometry } from 'three/examples/jsm/geometries/TextGeometry';
 import { gsap } from 'gsap';
 
-const STAR_SPREAD = 2000;
-const STAR_COUNT = 10000;
-const STAR_CENTER_1 = 200;
-const STAR_CENTER_2 = 800;
+const STAR_SPREAD_X = 1000;
+const STAR_SPREAD_Y = 1000;
+const STAR_SPREAD_Z = 5000;
+const STAR_COUNT = 50000;
+const STAR_CENTER = 1000;
 const STAR_SIZE = 0.5;
 
 const TIMELINE_START_POSITION = 0;
 const ASTEROID_START_POSITION = -100;
-const EARTH_POSITION = 600;
+const EARTH_POSITION = 3100;
 const EARTH_RADIUS = 80;
 
 const TEXT_VISIBILITY_THRESHOLD = 100;
 
 const SCROLL = {y: 0};
-const MAX_VELOCITY = 1;
+const MAX_VELOCITY = 5;
 const DAMPING_FACTOR = 0.94;
 
-const CAMERA_PARAMETERS = { fov: 45, near: 0.1, far: 5000};
+const MOUSE = { x: 0, y: 0 };
+const TITLE_SCREEN = { showing: true };
+const BUTTON = {clicked: false};
+
+const CAMERA_PARAMETERS = { fov: 45, near: 0.1, far: 4000};
 const CAMERA_INITIAL_POSITION = { x: 0, y: 200, z: -400 };
 const CAMERA_TO_ASTEROID_DISTANCE = 10;
 const CAMERA_FOCAL_LENGTH = 20;
 
+const SUN_POSITION = 3500;
+const SUN_RADIUS = 10;
 const SUNLIGHT = { color: 0x404040, intensity: 10 };
 const SUNLIGHT_POSITION = { x: -500, y: 0, z: -600 };
 
 const textData = [
-  { year: 1950, info: 'Information for 1950', position: new THREE.Vector3(0, 1, TIMELINE_START_POSITION) },
-  { year: 1960, info: 'Information for 1960', position: new THREE.Vector3(10, 1, TIMELINE_START_POSITION + 50) },
-  { year: 1970, info: 'Information for 1970', position: new THREE.Vector3(0, 1, TIMELINE_START_POSITION + 100) },
-  { year: 1980, info: 'Information for 1980', position: new THREE.Vector3(10, 1, TIMELINE_START_POSITION + 150) },
-  { year: 1990, info: 'Information for 1990', position: new THREE.Vector3(0, 1, TIMELINE_START_POSITION + 200) },
-  { year: 2000, info: 'Information for 2000', position: new THREE.Vector3(10, 1, TIMELINE_START_POSITION + 250) },
-  { year: 2010, info: 'Information for 2010', position: new THREE.Vector3(0, 1, TIMELINE_START_POSITION + 300) },
-  { year: 2020, info: 'Information for 2020', position: new THREE.Vector3(10, 1, TIMELINE_START_POSITION + 350) },
-  { year: 2030, info: 'Information for 2030', position: new THREE.Vector3(0, 1, TIMELINE_START_POSITION + 400) },
-  { year: 2040, info: 'Information for 2040', position: new THREE.Vector3(10, 1, TIMELINE_START_POSITION + 450) },
-  { year: 2050, info: 'Information for 2050', position: new THREE.Vector3(0, 1, TIMELINE_START_POSITION + 500) }
+  { year: 1950, info: 'Information for 1950', position: new THREE.Vector3(-5, 1, 0) },
+  { year: 1960, info: 'Information for 1960', position: new THREE.Vector3(15, 1, 500) },
+  { year: 1970, info: 'Information for 1970', position: new THREE.Vector3(-5, 1, 1000) },
+  { year: 1980, info: 'Information for 1980', position: new THREE.Vector3(15, 1, 1500) },
+  { year: 1990, info: 'Information for 1990', position: new THREE.Vector3(-5, 1, 2000) },
+  { year: 2000, info: 'Information for 2000', position: new THREE.Vector3(15, 1, 2500) },
+  { year: 2010, info: 'Information for 2010', position: new THREE.Vector3(-5, 1, 2600) },
+  { year: 2020, info: 'Information for 2020', position: new THREE.Vector3(15, 1, 2700) },
+  { year: 2030, info: 'Information for 2030', position: new THREE.Vector3(-5, 1, 2800) },
+  { year: 2040, info: 'Information for 2040', position: new THREE.Vector3(15, 1, 2900) },
+  { year: 2050, info: 'Information for 2050', position: new THREE.Vector3(-5, 1, 2950) }
 ];
 
 const cameraData = [
-  { distance: 0, positionOffset: new THREE.Vector3(0, 0, 0), lookAt: new THREE.Vector3(0, 0, 0) },
-  { distance: 0, positionOffset: new THREE.Vector3(0, 0, 0), lookAt: new THREE.Vector3(0, 0, 0) },
-  { distance: 0, positionOffset: new THREE.Vector3(0, 0, 0), lookAt: new THREE.Vector3(0, 0, 0) }
+  { distance: -1000, positionOffset: new THREE.Vector2(0, 0), lookAt: new THREE.Vector3(0, 0, 0) }
 ];
 
 function createText(scene) {
@@ -137,24 +142,36 @@ function updateText(camera, texts) {
 }
 
 function createStars(scene, center) {
-  const starsGeometry = new THREE.BufferGeometry();
-  const starsMaterial = new THREE.PointsMaterial({ color: 0xFFFFFF, size: STAR_SIZE});
+  return new Promise((resolve, reject) => {
+    try {
+      const starsGeometry = new THREE.BufferGeometry();
+      const starsMaterial = new THREE.PointsMaterial({ color: 0xFFFFFF, size: STAR_SIZE});
 
-  const starPoints = [];
+      const starPoints = [];
 
-  for (let i = 0; i < STAR_COUNT; i++) {
-      const x = THREE.MathUtils.randFloatSpread(STAR_SPREAD);
-      const y = THREE.MathUtils.randFloatSpread(STAR_SPREAD);
-      const z = THREE.MathUtils.randFloatSpread(STAR_SPREAD) + center;
+      for (let i = 0; i < STAR_COUNT; i++) {
+        const x = THREE.MathUtils.randFloatSpread(STAR_SPREAD_X);
+        const y = THREE.MathUtils.randFloatSpread(STAR_SPREAD_Y);
+        const z = THREE.MathUtils.randFloatSpread(STAR_SPREAD_Z) + center;
 
-      starPoints.push(x, y, z);
-  }
+        starPoints.push(x, y, z);
+      }
 
-  starsGeometry.setAttribute('position', new THREE.Float32BufferAttribute(starPoints, 3));
+      starsGeometry.setAttribute('position', new THREE.Float32BufferAttribute(starPoints, 3));
 
-  const stars = new THREE.Points(starsGeometry, starsMaterial); 
+      const stars = new THREE.Points(starsGeometry, starsMaterial); 
 
-  scene.add(stars);
+      scene.add(stars);
+
+      resolve(stars);
+    } catch (error) {
+      reject(error);
+    }
+  });
+}
+
+function updateStars(starCluster, asteroid) { 
+  return;
 }
 
 function createAsteroid(scene, loader) {
@@ -177,7 +194,7 @@ function createAsteroid(scene, loader) {
   });
 }
 
-function updateAsteroidPosition(asteroid, earth, camera, titleScreen) {
+function updateAsteroidPosition(asteroid) {
   const moveAmount = calculateMoveAmount();
   asteroid.position.z += moveAmount;
 
@@ -188,38 +205,62 @@ function updateAsteroidPosition(asteroid, earth, camera, titleScreen) {
   if (asteroid.position.z > EARTH_POSITION){
     asteroid.position.z = EARTH_POSITION;
   }
+}
 
-  if (!titleScreen.showing) {
-    camera.lookAt(earth.position.x, earth.position.y, earth.position.z);
-    
-    let newPosition = {x: asteroid.position.x, y: asteroid.position.y,};
-
+function updateCamera(asteroid, earth, camera) {
+  
+  if (!TITLE_SCREEN.showing) {
     camera.position.z = asteroid.position.z - CAMERA_TO_ASTEROID_DISTANCE;
+    camera.position.y = asteroid.position.y + 2;
+  
+    let newPosition = { x: asteroid.position.x, y: asteroid.position.y };
+    let lookAtPosition = new THREE.Vector3(earth.position.x, earth.position.y, earth.position.z);
 
-    for (let i = 0; i < textData.length; i++) {
-      const textPosition = textData[i].position;
-      
-      if (asteroid.position.z > (textPosition.z - 30)) {
-        if (i % 2 === 0) {
-          newPosition.x = asteroid.position.x - 4;
-          newPosition.y = asteroid.position.y;
-        } else {
-          newPosition.x = asteroid.position.x + 4;
-          newPosition.y = asteroid.position.y;
+    for (let i = 0; i < cameraData.length; i++) {
+      const cameraInfo = cameraData[i];
+      if (Math.abs(asteroid.position.z - cameraInfo.distance) <= 1) {
+        newPosition.x += cameraInfo.positionOffset.x;
+        newPosition.y += cameraInfo.positionOffset.y;
+        if (cameraInfo.lookAt) {
+          lookAtPosition = cameraInfo.lookAt;
         }
+        break;
       }
     }
-
-    gsap.to(camera.position, { 
-      duration: 5, 
-      x: newPosition.x, 
+    
+    gsap.to(camera.position, {
+      duration: 5,
+      x: newPosition.x,
       y: newPosition.y,
-      ease: "power1", 
-      onUpdate: function() {
-        camera.lookAt(earth.position.x, earth.position.y, earth.position.z);
+      ease: "power1",
+      onUpdate: function () {
+        camera.lookAt(lookAtPosition.x, lookAtPosition.y, lookAtPosition.z);
       }
     });
   }
+}
+
+function updateInitialCamera(camera, asteroid, earth) {
+  const newPosition = {
+    x: asteroid.position.x,
+    y: asteroid.position.y+2,
+    z: asteroid.position.z - CAMERA_TO_ASTEROID_DISTANCE
+  };
+
+  gsap.to(camera.position, { 
+    duration: 2, 
+    x: newPosition.x, 
+    y: newPosition.y, 
+    z: newPosition.z,
+    ease: "power4", 
+    onUpdate: function() {
+      camera.lookAt(earth.position.x, earth.position.y, earth.position.z);
+    },
+    onComplete: function() {
+      // camera.lookAt(earth.position);
+      TITLE_SCREEN.showing = false;
+    } 
+  });
 }
 
 function updateAsteroidRotation(asteroid) {
@@ -280,9 +321,98 @@ function createEarth(scene, loader) {
   });
 }
 
+// function createEarth(scene, loader) {
+//   return new Promise((resolve, reject) => {
+//     const atmosphereVertexShader = `
+//     varying vec3 vNormal;
+//     void main() {
+//       vNormal = normalize( normalMatrix * normal );
+//       gl_Position = projectionMatrix * modelViewMatrix * vec4( position, 1.0 );
+//     }
+//     `;
+//     const atmosphereFragmentShader = `
+//     varying vec3 vNormal;
+//     uniform float opacity;
+//     void main() {
+//       float intensity = pow( 0.7 - dot( vNormal, vec3( 0.0, 0.0, 1.0 ) ), 2.0 );
+//       gl_FragColor = vec4( 0.2, 0.6, 1.0, opacity ) * intensity;
+//     }
+//     `;
+
+//     const earthGeometry = new THREE.SphereGeometry(EARTH_RADIUS, 32, 32);
+    
+//     // Lava shader
+//     const lavaVertexShader = `
+//     varying vec2 vUv;
+//     void main() {
+//       vUv = uv;
+//       gl_Position = projectionMatrix * modelViewMatrix * vec4(position, 1.0);
+//     }
+//     `;
+//     const lavaFragmentShader = `
+//     varying vec2 vUv;
+//     void main() {
+//       float lavaColor = 0.5 + 0.5 * sin(vUv.y * 10.0 + time);
+//       gl_FragColor = vec4(vec3(lavaColor), 1.0);
+//     }
+//     `;
+
+//     const earthMaterial = new THREE.ShaderMaterial({
+//       uniforms: {
+//         time: { value: 0.0 }
+//       },
+//       vertexShader: lavaVertexShader,
+//       fragmentShader: `
+//       uniform float time;
+//       varying vec2 vUv;
+//       void main() {
+//         float lavaColor = 0.5 + 0.5 * sin(vUv.y * 10.0 + time);
+//         gl_FragColor = vec4(vec3(lavaColor), 1.0);
+//       }
+//       `
+//     });
+
+
+//     const atmosphereGeometry = new THREE.SphereGeometry(EARTH_RADIUS * 1.1, 32, 32);
+//     const atmosphereMaterial = new THREE.ShaderMaterial({
+//       vertexShader: atmosphereVertexShader,
+//       fragmentShader: atmosphereFragmentShader,
+//       side: THREE.BackSide,
+//       blending: THREE.AdditiveBlending,
+//       transparent: true,
+//       uniforms: {
+//         opacity: { value: 0.2 }
+//       }
+//     });
+
+//     const earth = new THREE.Mesh(earthGeometry, earthMaterial);
+//     earth.position.set(0, 0, EARTH_POSITION);
+//     scene.add(earth);
+
+//     const atmosphere = new THREE.Mesh(atmosphereGeometry, atmosphereMaterial);
+//     scene.add(atmosphere);
+//     atmosphere.position.set(earth.position.x, earth.position.y, earth.position.z);
+
+//     resolve(earth);
+//   });
+// }
+
+
 function updateEarthRotation(earth) {
   earth.rotation.y += 0.001;
 }
+
+function createSun(scene, loader) {
+  const sunGeometry = new THREE.SphereGeometry(SUN_RADIUS, 10, 10);
+  const sunTexture = loader.load('/images/sun.jpeg');
+  const sunMaterial = new THREE.MeshBasicMaterial({ map: sunTexture });
+
+  const sun = new THREE.Mesh(sunGeometry, sunMaterial);
+  sun.position.set(420,-10, SUN_POSITION);
+  scene.add(sun);
+}
+
+
 
 function calculateMoveAmount() {
   SCROLL.y *= DAMPING_FACTOR;
@@ -295,11 +425,11 @@ function calculateMoveAmount() {
   return moveAmount;
 }
 
-function mouseInteraction(camera, mouse, button, titleScreen) {
+function mouseInteraction(camera) {
   const parallaxFactor = 0.1;
-  if (!button.clicked) {
-    let newX = camera.position.x + mouse.x * 0.5;
-    let newY = camera.position.y + mouse.y * 0.5;
+  if (!BUTTON.clicked) {
+    let newX = camera.position.x + MOUSE.x * 0.5;
+    let newY = camera.position.y + MOUSE.y * 0.5;
 
     newX = Math.max(Math.min(newX, 50), -50);
     newY = Math.max(Math.min(newY, 50), -50);
@@ -309,10 +439,10 @@ function mouseInteraction(camera, mouse, button, titleScreen) {
 
     return;
   } 
-  if (!titleScreen.showing) {
+  if (!TITLE_SCREEN.showing) {
     return;
-    // camera.position.x += mouse.x * parallaxFactor;
-    // camera.position.y += mouse.y * parallaxFactor;
+    // camera.position.x += MOUSE.x * parallaxFactor;
+    // camera.position.y += MOUSE.y * parallaxFactor;
   }
 }
 
@@ -320,29 +450,6 @@ function onWindowResize(camera, renderer) {
   camera.aspect = window.innerWidth / window.innerHeight;
   camera.updateProjectionMatrix();
   renderer.setSize(window.innerWidth, window.innerHeight);
-}
-
-function updateInitialCamera(camera, asteroid, earth, mouse, titleScreen) {
-  const newPosition = {
-    x: asteroid.position.x,
-    y: asteroid.position.y,
-    z: asteroid.position.z - CAMERA_TO_ASTEROID_DISTANCE
-  };
-
-  gsap.to(camera.position, { 
-    duration: 2, 
-    x: newPosition.x, 
-    y: newPosition.y, 
-    z: newPosition.z,
-    ease: "power4", 
-    onUpdate: function() {
-      camera.lookAt(earth.position.x, earth.position.y, earth.position.z);
-    },
-    onComplete: function() {
-      camera.lookAt(earth.position);
-      titleScreen.showing = false;
-    } 
-  });
 }
 
 function updateScrollHandle(asteroid){
@@ -379,15 +486,17 @@ function animateUI(){
   });
 }
 
-function animate(composer, scene, camera, asteroid, earth, texts, mouse, titleScreen, button) {
-  requestAnimationFrame(() => animate(composer, scene, camera, asteroid, earth, texts, mouse, titleScreen, button));
+function animate(composer, scene, camera, asteroid, earth, texts, stars) {
+  requestAnimationFrame(() => animate(composer, scene, camera, asteroid, earth, texts, stars));
 
-  updateAsteroidPosition(asteroid, earth, camera, titleScreen);
+  updateAsteroidPosition(asteroid);
+  updateStars(stars, asteroid);
+  updateCamera(asteroid, earth, camera);
   updateScrollHandle(asteroid);
   updateText(camera, texts);
   updateEarthRotation(earth);
   updateAsteroidRotation(asteroid);
-  mouseInteraction(camera,mouse, button, titleScreen);
+  mouseInteraction(camera);
 
   composer.render(scene, camera);
 }
@@ -396,9 +505,8 @@ async function main() {
   const scene = new THREE.Scene();
   const loader = new THREE.TextureLoader();
 
-  createStars(scene, STAR_CENTER_1);
-  createStars(scene, STAR_CENTER_2);
-
+  createSun(scene, loader);
+  const stars = await createStars(scene, STAR_CENTER);
   const text = await createText(scene);
   const asteroid = await createAsteroid(scene, loader);
   const earth = await createEarth(scene, loader);
@@ -416,7 +524,7 @@ async function main() {
 
   camera.position.set(CAMERA_INITIAL_POSITION.x, CAMERA_INITIAL_POSITION.y, CAMERA_INITIAL_POSITION.z);
   camera.setFocalLength(CAMERA_FOCAL_LENGTH);
-  camera.lookAt(asteroid.position.x, asteroid.position.y-2, asteroid.position.z+100);
+  camera.lookAt(earth.position.x-250, earth.position.y-175, earth.position.z);
 
   const canvas = document.querySelector(".webgl");
   const renderer = new THREE.WebGLRenderer({ canvas, antialias: true, logarithmicDepthBuffer: false  });
@@ -432,19 +540,15 @@ async function main() {
   composer.addPass(renderScene);
   // composer.addPass(afterimagePass);
 
-  const mouse = { x: 0, y: 0 };
-  const titleScreen = {showing: true};
-  const button = {clicked: false};
-
   window.addEventListener('mousemove', function(e) {
-    mouse.x = (e.clientX / window.innerWidth) * 2 - 1;
-    mouse.y = -(e.clientY / window.innerHeight) * 2 + 1;
+    MOUSE.x = (e.clientX / window.innerWidth) * 2 - 1;
+    MOUSE.y = -(e.clientY / window.innerHeight) * 2 + 1;
   });
 
   window.addEventListener('resize', () => onWindowResize(camera, renderer), false);
 
   window.addEventListener('wheel', function(e) {
-    if (!titleScreen.showing) {
+    if (!TITLE_SCREEN.showing) {
       SCROLL.y += e.deltaY;
     }
   });
@@ -459,13 +563,13 @@ async function main() {
 
     document.querySelector('.explore-button').style.display = 'none';
 
-    button.clicked = true;
-    updateInitialCamera(camera, asteroid, earth, mouse, titleScreen);
+    BUTTON.clicked = true;
+    updateInitialCamera(camera, asteroid, earth);
 
   });
 
   animateUI();
-  animate(composer, scene, camera, asteroid, earth, text, mouse, titleScreen, button);
+  animate(composer, scene, camera, asteroid, earth, text, stars);
 }
 
 main();
